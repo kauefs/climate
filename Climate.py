434ba0431d2271755f0ebce743f309826f0f53a2
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 16:24:49 -0300

    Climate.py

--- Climate.py
+++ Climate.py
@@ -27,14 +27,7 @@ def load_data():
     return    data
 df          = load_data()
 # SIDE:
-st.sidebar.title(   'ƊⱭȾɅViƧi🧿Ƞ')
-st.sidebar.markdown('''
-[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
-[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
-[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
-[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/github/license/kauefs/COVID?logo=mit&logoColor=FFFFFF&label=License&labelColor=8B959E&color=750014)](https://web.mit.edu/Saltzer/www/publications/MITLicense.pdf)
-            ''')
+st.sidebar.title(    'ƊⱭȾɅViƧi🧿Ƞ'   )
 st.sidebar.divider(                   )
 st.sidebar.success(  'Climate Warming')
 st.sidebar.info(     'Global  Change ')
@@ -44,7 +37,14 @@ st.sidebar.markdown('''Source: [CIIAGRO](https://ciiagro.sp.gov.br/) – tempera
                     .format(df.index.min(), df.index.max()))
 table       = st.sidebar.empty()
 st.sidebar.divider(            )
-st.sidebar.markdown('''2023.11.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+st.sidebar.markdown('''
+[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
+[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
+[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
+[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
+[![License]( https://img.shields.io/github/license/kauefs/COVID?logo=mit&logoColor=FFFFFF&label=License&labelColor=8B959E&color=750014)](https://web.mit.edu/Saltzer/www/publications/MITLicense.pdf)
+
+2023.11.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
 # MAIN:
 st.divider(                          )
 st.title(   'In Search of a Warming!')
